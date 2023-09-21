04a9d84235e66366d3c9af6cc08f11a47ba6bb4b
ibrahim.benli <ibrahim.benli@geos.com.tr>
2023-09-21 18:35:19 +0300

    Add project files.

---
+++ Exell.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ExcellDataReader
+{
+    partial class Exell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDataRead = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDataRead
+            // 
+            this.btnDataRead.Location = new System.Drawing.Point(85, 389);
+            this.btnDataRead.Name = "btnDataRead";
+            this.btnDataRead.Size = new System.Drawing.Size(320, 49);
+            this.btnDataRead.TabIndex = 0;
+            this.btnDataRead.Text = "btnDataRead";
+            this.btnDataRead.UseVisualStyleBackColor = true;
+            this.btnDataRead.Click += new System.EventHandler(this.btnDataRead_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(68, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(354, 355);
+            this.listBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(445, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(354, 355);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnDataRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDataRead;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Exell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace ExcellDataReader
+{
+    public partial class Exell : Form
+    {
+        OleDbConnection conn;
+        OleDbCommand cmd;
+        OleDbDataReader reader;
+        public Exell()
+        {
+            InitializeComponent();
+        }
+        List<string> persons = new List<string>();
+        private void btnDataRead_Click(object sender, EventArgs e)
+        {
+            using (conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\ibrahim.benli\Desktop\test.ods; Extended Properties='Ods 12.0 xml;HDR=no;'"))
+            {
+                //conn.Open();
+                OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM [Sayfa1$]", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt.DefaultView;
+                //conn.Close();
+            }
+        }
+    }
+}
